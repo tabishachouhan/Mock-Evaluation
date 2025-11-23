10dd744368e50475ae4c8361f5e35be53e8187d1
poshiplatta <tabishachouhan001@gmail.com>
2025-11-23 19:33:51 +0530

    mock evaluation

--- Part A index.js
+++ Part A index.js
@@ -12,6 +12,52 @@ let extra = {city: "Bengaluru" };
 let profile = {...person, ...extra};
 console.log(profile);
 
+/* Q3 :Rest Operator */
+
+function sumAll(...numbers) {
+    return numbers.reduce((total,num))
+}
+console.log(sumAll(1,2,3,4));
 
 
 
+/* Q4: Rest Operator */
+
+let numbers= [10,20,30,40,50];
+
+let [a, ...remaining] = numbers;
+
+console.log(a);
+console.log(remaining);
+
+/* Q5: Nested Object Destructuring
+ */
+
+let user= {
+    name: "Alice",
+    address: {
+        city: "Bengaluru",
+        pin: 560001,
+        geo: {lat: 11.22, lng: 77.33}
+
+    
+    }
+};
+
+const {
+    address: {city, geo: {lat,lng} =user;
+}
+}
+console.log(city);
+console.log(lat);
+console.log(lng);
+
+
+/* Q6: Arrow Function */
+
+function multiply(a,b) {
+    return a*b;
+}
+
+const multiply = (a,b) =>a*b;
+console.log(multiply(4,5));
